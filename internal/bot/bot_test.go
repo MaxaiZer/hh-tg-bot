1b2597e0b4303b0ecbca6638b1958d2b6937d6b2
MaxaiZer <max7zer@gmail.com>
2024-11-10 23:43:27 +0700

    add tests for invalid input and editSearchCommand

--- internal/bot/bot_test.go
+++ internal/bot/bot_test.go
@@ -17,7 +17,7 @@ type mockSearchRepo struct {
 	Searches []entities.JobSearch
 }
 
-func (m *mockSearchRepo) Update(ctx context.Context, search entities.JobSearch) error {
+func (m *mockSearchRepo) Update(_ context.Context, search entities.JobSearch) error {
 	for i := 0; i < len(m.Searches); i++ {
 		if m.Searches[i].ID == search.ID {
 			m.Searches[i] = search
@@ -27,11 +27,11 @@ func (m *mockSearchRepo) Update(ctx context.Context, search entities.JobSearch) 
 	return errors.New("not found")
 }
 
-func (m *mockSearchRepo) Get(ctx context.Context, pageSize int, pageNum int) ([]entities.JobSearch, error) {
+func (m *mockSearchRepo) Get(_ context.Context, _ int, _ int) ([]entities.JobSearch, error) {
 	return m.Searches, nil
 }
 
-func (m *mockSearchRepo) GetByUser(ctx context.Context, userID int64) ([]entities.JobSearch, error) {
+func (m *mockSearchRepo) GetByUser(_ context.Context, userID int64) ([]entities.JobSearch, error) {
 	result := make([]entities.JobSearch, 0)
 	for i := 0; i < len(m.Searches); i++ {
 		if m.Searches[i].UserID == userID {
@@ -41,16 +41,16 @@ func (m *mockSearchRepo) GetByUser(ctx context.Context, userID int64) ([]entitie
 	return result, nil
 }
 
-func (m *mockSearchRepo) Add(ctx context.Context, search entities.JobSearch) error {
+func (m *mockSearchRepo) Add(_ context.Context, search entities.JobSearch) error {
 	m.Searches = append(m.Searches, search)
 	return nil
 }
 
-func (m *mockSearchRepo) UpdateLastCheckedVacancy(ctx context.Context, id int, vacancy hh.VacancyPreview) error {
+func (m *mockSearchRepo) UpdateLastCheckedVacancy(_ context.Context, _ int, _ hh.VacancyPreview) error {
 	panic("implement me")
 }
 
-func (m *mockSearchRepo) Remove(ctx context.Context, ID int) error {
+func (m *mockSearchRepo) Remove(_ context.Context, ID int) error {
 	for i, search := range m.Searches {
 		if search.ID == ID {
 			m.Searches = append(m.Searches[:i], m.Searches[i+1:]...)
@@ -60,11 +60,11 @@ func (m *mockSearchRepo) Remove(ctx context.Context, ID int) error {
 }
 
 type mockApi struct {
-	SendedMessages []botApi.Chattable
+	SentMessages []botApi.Chattable
 }
 
 func (m mockApi) Send(chattable botApi.Chattable) (botApi.Message, error) {
-	m.SendedMessages = append(m.SendedMessages, chattable)
+	m.SentMessages = append(m.SentMessages, chattable)
 	return botApi.Message{}, nil
 }
 
@@ -72,13 +72,19 @@ type mockRegionRepo struct {
 	Regions []entities.Region
 }
 
-func (m *mockRegionRepo) GetIdByName(ctx context.Context, name string) (string, error) {
+func (m *mockRegionRepo) GetIdByName(_ context.Context, name string) (string, error) {
 	for _, region := range m.Regions {
 		if region.NormalizedName == entities.NormalizeRegionName(name) {
 			return region.ID, nil
 		}
 	}
 	return "", errors.New("not found")
+}
+
+func simulateUserInput(cmd command, inputs []string) {
+	for _, input := range inputs {
+		cmd.OnUserInput(input)
+	}
 }
 
 func Test_AddSearchCmd_WhenValidData_ShouldBeSuccessful(t *testing.T) {
@@ -88,7 +94,6 @@ func Test_AddSearchCmd_WhenValidData_ShouldBeSuccessful(t *testing.T) {
 	region := entities.NewRegion("0", "Москва")
 	mockSearches := &mockSearchRepo{}
 	mockRegions := &mockRegionRepo{Regions: []entities.Region{region}}
-	mockApi := &mockApi{}
 	finished := false
 
 	keywords := "C#"
@@ -97,18 +102,46 @@ func Test_AddSearchCmd_WhenValidData_ShouldBeSuccessful(t *testing.T) {
 	wish := "Хочу пельмени"
 	initialSearchPeriod := 1
 
-	cmd := newAddSearchCommand(mockApi, 0, mockSearches, mockRegions)
-	cmd.WithFinishCallback(func() {
-		finished = true
-	})
-	cmd.Run()
+	cmd := newAddSearchCommand(&mockApi{}, 0, mockSearches, mockRegions)
+	cmd.WithFinishCallback(func() { finished = true })
 
+	cmd.Run()
+	simulateUserInput(cmd, []string{keywords, experience, region.Name, schedule, wish, strconv.Itoa(initialSearchPeriod)})
+
+	assert.True(finished)
+	assert.True(len(mockSearches.Searches) == 1)
+	assert.Equal(mockSearches.Searches[0].SearchText, keywords)
+	assert.Equal(mockSearches.Searches[0].RegionID, region.ID)
+	assert.Equal(mockSearches.Searches[0].Experience, entities.NoExperience)
+	assert.Equal(mockSearches.Searches[0].UserWish, wish)
+	assert.Equal(mockSearches.Searches[0].InitialSearchPeriod, initialSearchPeriod)
+}
+
+func Test_AddSearchCmd_WhenInvalidInput_ShouldWaitForValid(t *testing.T) {
+
+	assert := assert.New(t)
+
+	region := entities.NewRegion("0", "Москва")
+	mockSearches := &mockSearchRepo{}
+	mockRegions := &mockRegionRepo{Regions: []entities.Region{region}}
+	finished := false
+
+	keywords := "C#"
+	experience := string(noExperience)
+	schedule := "0"
+	wish := "Хочу пельмени"
+	initialSearchPeriod := 1
+
+	cmd := newAddSearchCommand(&mockApi{}, 0, mockSearches, mockRegions)
+	cmd.WithFinishCallback(func() { finished = true })
+
+	cmd.Run()
 	cmd.OnUserInput(keywords)
-	cmd.OnUserInput(experience)
-	cmd.OnUserInput(region.Name)
-	cmd.OnUserInput(schedule)
+	simulateUserInput(cmd, []string{"justRandomExperience", experience})
+	simulateUserInput(cmd, []string{"justRandomRegion", region.Name})
+	simulateUserInput(cmd, []string{"-1", schedule})
 	cmd.OnUserInput(wish)
-	cmd.OnUserInput(strconv.Itoa(initialSearchPeriod))
+	simulateUserInput(cmd, []string{strconv.Itoa(-1), strconv.Itoa(6), strconv.Itoa(initialSearchPeriod)})
 
 	assert.True(finished)
 	assert.True(len(mockSearches.Searches) == 1)
@@ -123,25 +156,94 @@ func Test_RemoveSearchCmd_WhenValidData_ShouldBeSuccessful(t *testing.T) {
 
 	assert := assert.New(t)
 
-	search := entities.JobSearch{
-		ID:     0,
-		UserID: 0,
-	}
+	search := entities.JobSearch{ID: 0, UserID: 0}
 	mockSearches := &mockSearchRepo{Searches: []entities.JobSearch{search}}
-	published := false
+	eventPublished := false
 	mockBus := EventBus.New()
-	_ = mockBus.Subscribe(events.SearchDeletedTopic, func(event events.SearchDeleted) { published = true })
-	mockApi := &mockApi{}
+	_ = mockBus.Subscribe(events.SearchDeletedTopic, func(event events.SearchDeleted) { eventPublished = true })
 	finished := false
 
-	cmd := newRemoveSearchCommand(mockApi, search.UserID, mockBus, mockSearches)
-	cmd.WithFinishCallback(func() {
-		finished = true
-	})
+	cmd := newRemoveSearchCommand(&mockApi{}, search.UserID, mockBus, mockSearches)
+	cmd.WithFinishCallback(func() { finished = true })
+
 	cmd.Run()
+	cmd.OnUserInput("1") //search num
+
+	assert.True(finished)
+	assert.Empty(mockSearches.Searches)
+	assert.True(eventPublished)
+}
+
+func Test_RemoveSearchCmd_WhenInvalidInput_ShouldWaitForValid(t *testing.T) {
+
+	assert := assert.New(t)
+
+	search := entities.JobSearch{ID: 0, UserID: 0}
+	mockSearches := &mockSearchRepo{Searches: []entities.JobSearch{search}}
+	finished := false
+
+	cmd := newRemoveSearchCommand(&mockApi{}, search.UserID, EventBus.New(), mockSearches)
+	cmd.WithFinishCallback(func() { finished = true })
+
+	cmd.Run()
+	simulateUserInput(cmd, []string{"-1", "2", "1"}) //search num
 	cmd.OnUserInput("1")
 
 	assert.True(finished)
 	assert.Empty(mockSearches.Searches)
-	assert.True(published)
+}
+
+func Test_EditSearchCmd_WhenValidData_ShouldBeSuccessful(t *testing.T) {
+
+	assert := assert.New(t)
+
+	search := entities.JobSearch{ID: 0, UserID: 0}
+	mockSearches := &mockSearchRepo{Searches: []entities.JobSearch{search}}
+	eventPublished := false
+	mockBus := EventBus.New()
+	_ = mockBus.Subscribe(events.SearchEditedTopic, func(event events.SearchEdited) { eventPublished = true })
+	finished := false
+
+	cmd := newEditSearchCommand(&mockApi{}, search.UserID, mockBus, mockSearches)
+	cmd.WithFinishCallback(func() { finished = true })
+
+	newKeywords := "1C"
+	newWish := "вкалывать за копейки"
+
+	cmd.Run()
+	cmd.OnUserInput("1") //select search num
+	cmd.OnUserInput("0") //select changing of keywords
+	cmd.OnUserInput(newKeywords)
+
+	assert.True(eventPublished)
+	assert.False(finished)
+	assert.Equal(mockSearches.Searches[0].SearchText, newKeywords)
+
+	cmd.OnUserInput("1") //select changing of user wish
+	cmd.OnUserInput(newWish)
+
+	assert.False(finished)
+	assert.Equal(mockSearches.Searches[0].UserWish, newWish)
+}
+
+func Test_EditSearchCmd_WhenInvalidInput_ShouldWaitForValid(t *testing.T) {
+
+	assert := assert.New(t)
+
+	search := entities.JobSearch{ID: 0, UserID: 0}
+	mockSearches := &mockSearchRepo{Searches: []entities.JobSearch{search}}
+	finished := false
+
+	cmd := newEditSearchCommand(&mockApi{}, search.UserID, EventBus.New(), mockSearches)
+	cmd.WithFinishCallback(func() { finished = true })
+
+	newKeywords := "1C"
+
+	cmd.Run()
+	simulateUserInput(cmd, []string{"-1", "2", "1"}) //select search num
+	simulateUserInput(cmd, []string{"-1", "3", "0"}) //select changing of keywords
+	cmd.OnUserInput(newKeywords)
+
+	assert.False(finished)
+	assert.Equal(mockSearches.Searches[0].SearchText, newKeywords)
 }
